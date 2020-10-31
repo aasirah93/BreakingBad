a253094bfe5753d3f44ebeb5602451637daa4a63
Asifur Rahman <aasirah93@hotmail.co.uk>
2020-10-31 17:01:29 +0000

    Console log values

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -6,6 +6,28 @@ function App() {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
+  const fetchData = () => {fetch("https://www.breakingbadapi.com/api/characters")
+  .then(res => res.json())
+  .then(
+    (result) => {
+      
+      setItems(result);
+      setIsLoading(false);
+     
+    },
+    // Note: it's important to handle errors here
+    // instead of a catch() block so that we don't swallow
+    // exceptions from actual bugs in components.
+    
+  )}
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  console.log(items)
+
+
+
   return (
     <div className='container'>
       <Header/>
