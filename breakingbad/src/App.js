9a9dff33378b9a4c351e439c0540e78440646e29
Asifur Rahman <aasirah93@hotmail.co.uk>
2020-10-31 16:42:49 +0000

    Const items const isLoading

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -3,6 +3,9 @@ import Header from './components/Header'
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
   return (
     <div className='container'>
       <Header/>
