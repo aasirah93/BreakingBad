c542044afd1d0bcfb3d6590fbf5b03aa7b1124e1
Asifur Rahman <aasirah93@hotmail.co.uk>
2020-11-10 23:54:43 +0000

    Add search to app file

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header'
 import CharacterGrid from './components/CharacterGrid'
+import Search from './components/Search'
 import './App.css';
 
 function App() {
--- breakingbad/src/components/Search.js
+++ breakingbad/src/components/Search.js
@@ -3,7 +3,11 @@ import React, { useState } from 'react'
 export default function Search() {
     return (
         <section>
-            
+            <form>
+                <input type='text' 
+                className='form-control' 
+                placeholder='Search characters'/>
+            </form>
         </section>
     )
 }
